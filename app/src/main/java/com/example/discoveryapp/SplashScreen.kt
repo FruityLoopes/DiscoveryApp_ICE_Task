583514b4c4b93fd01e0dbc1f924042f2deb61d93
FruityLoopes <chadandrews07@gmail.com>
2023-08-10 10:08:01 +0200

    Discovery App

---
+++ app/src/main/java/com/example/discoveryapp/SplashScreen.kt
@@ -0,0 +1,27 @@
+package com.example.discoveryapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreen : AppCompatActivity() {
+
+    private val SPLASH_DURATION: Long = 5000 // 5 seconds
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        // Use a handler to delay the splash screen and then start the main activity
+        Handler().postDelayed({
+            // Start the main activity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+            // Close the splash activity so the user won't go back to it using the back button
+            finish()
+        }, SPLASH_DURATION)
+
+    }
+}
